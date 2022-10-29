846ccdad8c6678c9700c1a1237bb2bc1f5740c40
fortuna <avidia38@gmail.com>
2022-10-29 09:44:20 +0900

    first: navigation drawer

---
+++ lib/pages/updates_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UpdatesPage extends StatefulWidget {
+  UpdatesPage({Key? key}) : super(key: key);
+
+  @override
+  State<UpdatesPage> createState() => _UpdatesPageState();
+}
+
+class _UpdatesPageState extends State<UpdatesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Colors.yellow,
+    );
+  }
+}
